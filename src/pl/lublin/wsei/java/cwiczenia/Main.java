19452ba57b34be28b0afee888901e83b89af1c68
Maciej Kosakowski <kosakowski222@gmail.com>
2022-03-14 14:18:20 +0100

    Punkt 12, Dodawanie do kodu własnej klasy

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,25 +1,12 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-import java.util.Random;
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 
 public class Main {
 
     public static void main(String[] arys){
-        int[] liczby = new int[30];
-        Random rnd = new Random();
-
-        for (int i = 0; i < 30; i++)
-            liczby[i] = rnd.nextInt();
-
-        int mx = Integer.MIN_VALUE;
-        int mn = Integer.MAX_VALUE;
-        long avg = 0;
-        for (int l : liczby){
-            System.out.println(l);
-            if (l < mn) mn = 1;
-            if (l > mx) mx = 1;
-            avg += l;
-        }
-        System.out.printf("MIN = %d,MAX = %d, AVG = %f", mn,mx, (float)avg/liczby.length);
+        Account acc = new Account();
+        acc.setName("maciej Kosakowski");
+        System.out.println(acc.getName());
     }
 }
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -0,0 +1,24 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Account {
+
+    private String name;
+
+    public static String capitalize(String aStr)
+    {
+        String[] tokens = StringUtils.split(aStr);
+        for(int i = 0; i < tokens.length; i++)
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        return StringUtils.join(tokens,' ');
+    }
+    public void setName(String name) {
+        this.name = capitalize(name);
+    }
+
+    public String getName() {
+       return name;
+    }
+
+}
