ae463a3381570c67500a892ea33af33fec79c39a
Maciej Kosakowski <kosakowski222@gmail.com>
2022-03-14 11:18:34 +0100

    Punkt 7, System.out

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,7 +2,15 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
 
-    public static voiid main(String[] arys){
-        System.out.println("Hello Java world\n");
+    public static void main(String[] arys){
+        int a = 3;
+        double b = 4.21;
+        String s = "jakis tekst";
+
+        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
+
+        System.out.printf("alfa\tsin(alfa) \n");
+        for (int i = 0; i < 370; i +=10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
     }
 }
