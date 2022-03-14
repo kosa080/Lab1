f7f5edb49efc465b0131c51248ed94afa1a6d825
Maciej Kosakowski <kosakowski222@gmail.com>
2022-03-14 11:34:21 +0100

    Punkt 9, Przekształcenie programu na pętlę

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] arys){
         Scanner input = new Scanner(System.in);
-        int num1, num2;
-        System.out.print("Podaj pierwsza liczbe: ");
-        num1 = input.nextInt();
-        System.out.print("Podaj druga liczbe: ");
-        num2 = input.nextInt();
-        System.out.printf("Wynik dodawania %d + %d = %d%n",num1,num2,num1+num2);
+        int num1=0, num2=0;
+        do {
+            System.out.print("Podaj liczby, ktore mam dodac: ");
+            num1 = input.nextInt();
+            num2 = input.nextInt();
+            if ((num1 == 0) || (num2 == 0)) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
+        } while (true);
     }
 }
